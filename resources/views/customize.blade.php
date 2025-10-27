cd2146af1f9c363d50ceefc55e3336fd39b9b441
Arkmers <arkmers@gmail.com>
2025-10-28 04:23:49 +0800

    Added the header text for customization page, still learning on how things work with tailwind

--- resources/views/customize.blade.php
+++ resources/views/customize.blade.php
@@ -1,3 +1,21 @@
 <x-layout>
-    
+    <div>
+        <h2 class="text-4xl font-fredoka font-bold mb-8">Design your Beaded Bracelet<h2>
+        Personalize your own bracelet using our very own customization tool.
+    </div>
+    <br> <br>
+    <div class="w-270 h-150 bg-gray-200 rounded-xl shadow-md flex items-center justify-center">
+  <p class="text-gray-700 font-medium">nagaaral pa po ako ng tailwind TvT + figuring out what to do for the customization</p>
+</div>
+    </div>
 </x-layout>
+
+<ul>
+    Todo:
+    <li>> Learn CSS and Tailwind in order to complete customization page.
+    <li>> Figure out how to implement the customization bracelet maker with JS (i think its js at least)
+    <li>> Finalize the design for what to do in customization page.
+    <li>> make it look good :D
+</ul>
+
+most of these are placeholders rn, will update when i make progress soon. its 4am and i need sleep for sts later rofl

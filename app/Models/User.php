0cf289b01ae682b9fdb68ae07f9483b24fd6686b
jeyem07 <jmtuba69@gmail.com>
2025-11-03 00:11:57 +0800

    lastt

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,37 +12,13 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The table associated with the model.
-     */
     protected $table = 'useraccount';
 
-    /**
-     * The primary key associated with the table.
-     */
     protected $primaryKey = 'user_id';
 
-    /**
-     * Indicates if the model's ID is auto-incrementing.
-     */
-    public $incrementing = true; // Set false if primary key is non-integer
-
-    /**
-     * The data type of the auto-incrementing ID.
-     */
+    public $incrementing = true;
     protected $keyType = 'int'; // set 'string' if non-int
-
-    /**
-     * Indicates if the model should be timestamped.
-     */
-    public $timestamps = true; // set to true if created_at/updated_at exist
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * We are using the list from the 'current' (main) branch as it
-     * includes 'phone' and 'birthday' which seem like the latest additions.
-     */
+    public $timestamps = true;
     protected $fillable = [
         'fullName',
         'email',
@@ -50,23 +26,11 @@ class User extends Authenticatable
         'birthday',
         'password',
         'status',
-        // 'user_id' is not needed in fillable if it's an auto-incrementing primary key
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * This combines the casts from both versions, using the modern
-     * function syntax from your stashed changes.
-     */
     protected function casts(): array
     {
         return [
@@ -74,12 +38,6 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
-    /**
-     * Get the cart associated with the user.
-     *
-     * This function was added from your stashed changes.
-     */
     public function cart()
     {
         return $this->hasOne(Cart::class, 'user_id');
